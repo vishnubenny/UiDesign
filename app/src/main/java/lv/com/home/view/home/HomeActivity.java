922c01c7dde165491685ca1164c13c96d5cd3195
vishnu <vishnu.benny@citrusinformatics.com>
2017-07-21 19:34:02 +0530

    Added click event for navigation menu

--- app/src/main/java/lv/com/home/view/home/HomeActivity.java
+++ app/src/main/java/lv/com/home/view/home/HomeActivity.java
@@ -1,6 +1,7 @@
 package lv.com.home.view.home;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -8,13 +9,15 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import lv.com.home.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
@@ -44,6 +47,7 @@ public class HomeActivity extends AppCompatActivity {
         };
         mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+        mNavigationView.setNavigationItemSelectedListener(this);
     }
 
     private void cacheWidget() {
@@ -52,5 +56,12 @@ public class HomeActivity extends AppCompatActivity {
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout_root_parent);
         mNavigationView = (NavigationView) findViewById(R.id.navigation_drawer);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        mDrawerLayout.closeDrawer(Gravity.START);
+        Toast.makeText(this, "Clicked on " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
